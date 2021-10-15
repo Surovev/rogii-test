676c445524b31c76a1e01ae03df46548f7c145dc
Surovev <dmitriysurovev@gmail.com>
2021-10-15 19:32:03 +0300

    sort wip

--- rogii-test/src/App.js
+++ rogii-test/src/App.js
@@ -1,19 +1,83 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {createStore} from 'redux';
+import {useDispatch, useSelector} from 'react-redux';
 import Line from './elements/Line';
+import LineTitle from './elements/LIneTitle';
 import './App.css';
 import testData from '../src/data/test-data.json';
+
 const data = testData.content.map((obj, i) => {obj.number = i + 1;
-return obj});
-console.log(data);
+  return obj});
+
+// сортировка   
+  function dynamicSort(property) {
+    var sortOrder = 1;
+    if(property[0] === "-") {
+        sortOrder = -1;
+        property = property.substr(1);
+    }
+    return function (a,b) {
+        /* next line works with strings and numbers, 
+         * and you may want to customize it to your needs
+         */
+        var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+        return result * sortOrder;
+    }
+}
+// сортировка
+
+// редьюсер
+
+const reducer = (state = data, action) => {
+
+  switch (action.type) {
+
+    case "COUNT_TABLE":
+      return[...state.slice(3,8)]
+
+    case "SORT":
+      console.log(action);
+      return [...state.sort( dynamicSort(action.payload))]
+
+    default:
+      return state;
+  }
+
+}
+export const store = createStore(reducer)
+
+
+
+// редьюсер
 
 function App() {
+
+  const [column, setColumn] = React.useState('');
+
+    const dispatch = useDispatch();
+    const renderData = useSelector(dataForRender => dataForRender)
+    console.log(renderData);
+
+    
+ function sortData(options) {
+  dispatch({type:"SORT", payload: options});
+}
+
+    const testFunc = () => {
+      dispatch({type:"COUNT_TABLE"});
+    }
+
+
   return (
     <div className="App">
       <h1 className="title">Table</h1>
 
       <table className="table">
-        <Line
+        <LineTitle
+          column={column}
+          setColumn={setColumn}
           isTitle
+          sortData={sortData}
           number="№"
           revision="revision"
           revstmp="revstmp"
@@ -26,8 +90,9 @@ function App() {
           changes_summary="changes_summary"
         />
 
-        {data.map(item => (
+        { renderData.map(item => (
          <Line
+          key={item.number}
           isTitle={false}
           number={item.number}
           revision={item.revision}
@@ -44,8 +109,8 @@ function App() {
       <div className="breadcrumps">
         <div className="results-per-page">
           <span className="results-per-page__title">На странице</span>
-          <span className="results-per-page__value">5</span>
-          <span className="results-per-page__value is-active">10</span>
+          <span className="results-per-page__value" onClick={() => testFunc()}>5</span>
+          <span className="results-per-page__value is-active" >10</span>
           <span className="results-per-page__value">25</span>
           <span className="results-per-page__value">50</span>
         </div>
---
+++ rogii-test/src/elements/LIneTitle.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+
+function LineTitle (props) {
+    
+    const [sort, setSort] = React.useState('-');
+    
+
+    const testSort = (value, e) => {
+        if (props.column !== '') {
+            props.column.classList.remove("arrow-up");
+            props.column.classList.remove("arrow-down");
+        }
+        props.setColumn(e.target);
+       // e.target.className.add('arrow-up');
+       
+        if (sort === '-') {
+            
+            props.setColumn(e.target);
+            console.log(props.column);
+            setSort('');
+            e.target.classList.add("arrow-up");
+            e.target.classList.remove("arrow-down");
+        }
+        if (sort === '') {
+            e.target.classList.add("arrow-down");
+            e.target.classList.remove("arrow-up");
+            setSort('-');
+        } 
+ 
+        props.sortData(`${sort}${value}`)
+    }
+
+    return (
+     <>
+        <tr className="table__row">
+          <td className={`table__value table__title-value `} onClick={(e) => { testSort('number', e)}}>{props.number}</td>
+          <td className={`table__value table__title-value `} onClick={(e) => testSort(props.revision, e)}>{props.revision}</td>
+          <td className="table__value table__title-value" onClick={(e) => testSort(props.revstmp, e)}>{props.revstmp}</td>
+          <td className="table__value table__title-value">{props.user_id}</td>
+          <td className="table__value table__title-value">{props.user_email}</td>
+          <td className="table__value table__title-value">{props.user_name}</td>
+          <td className="table__value table__title-value">{props.well_id}</td>
+          <td className="table__value table__title-value">{props.well_name}</td>
+          <td className="table__value table__title-value">{props.well_type}</td>
+          <td className="table__value table__title-value">{props.changes_summary}</td>
+        </tr>
+     </>
+    );
+}
+
+export default LineTitle;
+
+//  arrow-up arrow-down
